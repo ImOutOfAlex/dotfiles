ea5e9a2688f71780153a4fb02ef588d0ac52b988
Alex Robinson <alex.robnson@gmail.com>
2017-08-30 00:17:07 -0700

    Fix/i3 (#8)
    * Fix minor pip issue.
    
    * Re-arrange bar so it has better usability.
    
    * Fix shell scripts to have the right modifiers.
    
    * Fix themes pathing so Xresources can handle it correctly
    
    * Remove the stupid tray output line which always breaks things.
    
    * Move X stuff around, change to use reload instead of restart.
    
    * Fix pathing issues.
    
    * Removed a debug line.
    
    * Rename due to folder.
    
    * Add multi-file i3 config.
    
    * Add xsession since that's where we need to reconfigure.
    
    * Add the merge config stuff again.
    
    * Fix merging configs.
    
    * Fix the xrdb stuff not working.
    
    * Reorganize i3 config stuff.

---
+++ i3/scripts/merge_configs.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env sh
+
+SCRIPT=$(readlink -f "${HOME}/.i3")
+BASEDIR=$(dirname $SCRIPT)
+DOTFILESDIR=$(realpath "${BASEDIR}")
+
+I3_DIR=$(realpath "${DOTFILESDIR}/i3")
+I3_CONFIG=$(realpath "${I3_DIR}/config")
+I3_CONFIG_DIR=$(realpath "${I3_DIR}/config.d")
+
+FORMAT_STRING="# This is an auto generated file. Do not modify.\n"
+
+if [ -d $I3_CONFIG_DIR ]; then
+    echo $FORMAT_STRING > $I3_CONFIG
+    cat $I3_CONFIG_DIR/* >> $I3_CONFIG
+fi

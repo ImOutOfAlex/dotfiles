06493720ca16a07c555ff13fa8c9caad05d3c0db
Alex <ImOutOfAlex@her.farm>
2017-10-19 21:39:58 -0700

    Add new keybind, and adjust old one.

--- zsh/keybinds.zsh
+++ zsh/keybinds.zsh
@@ -4,7 +4,7 @@ bindkey "\e[H" beginning-of-line
 bindkey "\e[7~" beginning-of-line
 
 bindkey "\e[F" end-of-line
-bindkey "\e[8" end-of-line
+bindkey "\e[8~" end-of-line
 
 bindkey "\e[3~" delete-char
 
@@ -13,6 +13,8 @@ bindkey "\e[2~" quoted-insert
 bindkey "\e[1;5C" forward-word
 
 bindkey "\e[1;5D" backward-word
+
+bindkey "^[[3^" forward-delete-word
 
 bindkey "^H" backward-delete-word
 

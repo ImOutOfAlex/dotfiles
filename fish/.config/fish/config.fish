251e40c93ac87da840d563cf43f7e47fa480bc9a
Alex <ImOutOfAlex@her.farm>
2019-09-07 18:47:15 -0700

    Move files around, add fish config, switch to gnu stow support.

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,16 @@
+#!/usr/bin/env fish
+
+if functions -q bass
+    bass source /etc/profile
+    bass source <(plz --completion_script)
+end
+
+if not functions -q fisher
+    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
+    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
+    fish -c fisher
+end
+
+set PATH $HOME/bin $PATH
+set PATH $HOME/.please $PATH
+
---
+++ fish/.config/fish/functions/dir.fish
@@ -0,0 +1,4 @@
+# Defined in - @ line 1
+function dir --description 'alias dir=ls -l'
+	ls -l $argv;
+end

db352c8328524f41ecfd190ff39502894c15518d
Alex <ImOutOfAlex@her.farm>
2019-09-12 22:45:43 -0700

    Remove z!

--- fish/.config/fish/functions/fish_prompt.fish
+++ fish/.config/fish/functions/fish_prompt.fish
@@ -1,104 +1,113 @@
 function fish_prompt
-	
-    # - green lines if the last return command is OK, red otherwise
-    # - your user name, in red if root or yellow otherwise
-    # - your hostname, in cyan if ssh or blue otherwise
-    # - the current path (with prompt_pwd)
-    # - date +%X
-    # - the current virtual environment, if any
-    # - the current git status, if any, with __fish_git_prompt
-    # - the current battery state, if any, and if your power cable is unplugged, and if you have "acpi"
-    # - current background jobs, if any
+    set -l status_copy $status
+    set -l pwd_info (pwd_info "/")
+    set -l dir
+    set -l base
+    set -l color (set_color white)
+    set -l color2 (set_color normal)
+    set -l color3 (set_color $fish_color_command)
+    set -l color_error (set_color $fish_color_error)
+    set -l color_normal "$color2"
 
-    # It goes from:
-    # ┬─[nim@Hattori:~]─[11:39:00]
-    # ╰─>$ echo here
+    echo -sn " "
 
-    # To:
-    # ┬─[nim@Hattori:~/w/dashboard]─[11:37:14]─[V:django20]─[G:master↑1|●1✚1…1]─[B:85%, 05:41:42 remaining]
-    # │ 2	15054	0%	arrêtée	sleep 100000
-    # │ 1	15048	0%	arrêtée	sleep 100000
-    # ╰─>$ echo there
-
-    set -q __fish_git_prompt_showupstream
-    or set -g __fish_git_prompt_showupstream auto
-
-    function _nim_prompt_wrapper
-        set retc $argv[1]
-        set field_name $argv[2]
-        set field_value $argv[3]
-
-        set_color normal
-        set_color $retc
-        echo -n '─'
-        set_color -o green
-        echo -n '['
-        set_color normal
-        test -n $field_name
-        and echo -n $field_name:
-        set_color $retc
-        echo -n $field_value
-        set_color -o green
-        echo -n ']'
+    if test "$status_copy" -ne 0
+        set color "$color_error"
+        set color2 "$color_error"
+        set color3 "$color_error"
     end
-    and set retc green
-    or set retc red
 
-    set_color $retc
-    echo -n '┬─'
-    set_color -o green
-    echo -n [
-    if test "$USER" = root -o "$USER" = toor
-        set_color -o red
+    set -l glyph " $color2\$$color_normal"
+
+    if test 0 -eq (id -u "$USER")
+        echo -sn "$color_error# $color_normal"
+    end
+
+    if test ! -z "$SSH_CLIENT"
+        set -l color "$color2"
+
+        if test 0 -eq (id -u "$USER")
+            set color "$color_error"
+        end
+
+        echo -sn "$color"(host_info "user@")"$color_normal"
+    end
+
+    if test "$PWD" = ~
+        set base "$color3~"
+        set glyph
+        
+    else if pwd_is_home
+        set dir
+
     else
-        set_color -o yellow
+        if test "$PWD" = /
+            set glyph
+        else
+            set dir "/"
+        end
+
+        set base "$color_error/"
     end
-    echo -n $USER
-    set_color -o white
-    echo -n @
-    if [ -z "$SSH_CLIENT" ]
-        set_color -o blue
+
+    if test ! -z "$pwd_info[1]"
+        set base "$pwd_info[1]"
+    end
+
+    if test ! -z "$pwd_info[2]"
+        set dir "$dir$pwd_info[2]/"
+    end
+
+    echo -sn "$color2$dir$color$base$color_normal"
+
+    if test ! -z "$pwd_info[3]"
+        echo -sn "$color2/$pwd_info[3]"
+    end
+
+    if set branch_name (git_branch_name)
+        set -l git_color
+        set -l git_glyph \$
+
+        if git_is_staged
+            set git_color (set_color green)
+
+            if git_is_dirty
+                set git_glyph "$git_color$git_glyph$color_error$git_glyph"
+                set git_color "$color_error"
+            end
+
+        else if git_is_dirty
+            set git_color "$color_error"
+
+        else if git_is_touched
+            set git_color "$color_error"
+        else
+            set git_color "$color3"
+        end
+
+        set -l git_ahead (git_ahead " +" " -" " +-")
+
+        if test "$branch_name" = "master"
+            set branch_name
+            if git_is_stashed
+                set branch_name "{}"
+            end
+        else
+            set -l left_par "("
+            set -l right_par ")"
+
+            if git_is_stashed
+                set left_par "{"
+                set right_par "}"
+            end
+
+            set branch_name " $git_color$left_par$color2$branch_name$git_color$right_par"
+        end
+
+        echo -sn "$branch_name$git_color$git_ahead $git_glyph"
     else
-        set_color -o cyan
+        echo -sn "$color$glyph$color_normal"
     end
-    echo -n (prompt_hostname)
-    set_color -o white
-    echo -n :(prompt_pwd)
-    set_color -o green
-    echo -n ']'
 
-    # Date
-    _nim_prompt_wrapper $retc '' (date +%X)
-
-    # Virtual Environment
-    set -q VIRTUAL_ENV
-    and _nim_prompt_wrapper $retc V (basename "$VIRTUAL_ENV")
-
-    # git
-    set prompt_git (__fish_git_prompt | string trim -c ' ()')
-    test -n "$prompt_git"
-    and _nim_prompt_wrapper $retc G $prompt_git
-
-    # Battery status
-    type -q acpi
-    and test (acpi -a 2> /dev/null | string match -r off)
-    and _nim_prompt_wrapper $retc B (acpi -b | cut -d' ' -f 4-)
-
-    # New line
-    echo
-
-    # Background jobs
-    set_color normal
-    for job in (jobs)
-        set_color $retc
-        echo -n '│ '
-        set_color brown
-        echo $job
-    end
-    set_color normal
-    set_color $retc
-    echo -n '╰─>'
-    set_color -o red
-    echo -n '$ '
-    set_color normal
+    echo -sn "$color_normal "
 end

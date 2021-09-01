54c944fbb96e2b869ada48b0952b39b6989cfbcd
Alex <ImOutOfAlex@her.farm>
2021-09-01 00:50:10 -0700

    Remove fisherman.

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -5,12 +5,6 @@ if functions -q bass
     # bass source <(plz --completion_script)
 end
 
-if not functions -q fisher
-    set -q XDG_CONFIG_HOME; or set XDG_CONFIG_HOME ~/.config
-    curl https://git.io/fisher --create-dirs -sLo $XDG_CONFIG_HOME/fish/functions/fisher.fish
-    fish -c fisher
-end
-
 update-colors-from-x
 
 if status --is-login

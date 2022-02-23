184fb04c93f7a078b497e47daebc4e632ee24cde
Alex <ImOutOfAlex@her.farm>
2022-02-23 10:26:31 -0800

    Add dap.

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -73,8 +73,55 @@ require('packer').startup(function(use)
   use 'jiangmiao/auto-pairs'  -- vimscript
   use 'tpope/vim-vinegar'  -- vimscript
   use 'tpope/vim-fugitive'  -- vimscript
-  use 'tpope/vim-dispatch'  -- vimscript
   use 'tpope/vim-repeat'  -- vimscript
+  use 'jbyuki/one-small-step-for-vimkind'
+  use 'mfussenegger/nvim-dap'
+  use {
+    "rcarriga/nvim-dap-ui",
+    requires = { "mfussenegger/nvim-dap", },
+    config = function ()
+      require("dapui").setup({
+        icons = { expanded = "▾", collapsed = "▸" },
+        sidebar = {
+          -- You can change the order of elements in the sidebar
+          elements = {
+            -- Provide as ID strings or tables with "id" and "size" keys
+            {
+              id = "scopes",
+              size = 0.25, -- Can be float or integer > 1
+            },
+            { id = "breakpoints", size = 0.25 },
+            { id = "stacks", size = 0.25 },
+            { id = "watches", size = 00.25 },
+          },
+          size = 40,
+          position = "left", -- Can be "left", "right", "top", "bottom"
+        },
+        tray = {
+          elements = { "repl" },
+          size = 10,
+          position = "bottom", -- Can be "left", "right", "top", "bottom"
+        },
+        floating = {
+          max_height = nil, -- These can be integers or a float between 0 and 1.
+          max_width = nil, -- Floats will be treated as percentage of your screen.
+          border = "single", -- Border style. Can be "single", "double" or "rounded"
+          mappings = {
+            close = { "q", "<Esc>" },
+          },
+        },
+        windows = { indent = 1 },
+      })
+    end
+  }
+  use {
+    "Pocco81/DAPInstall.nvim",
+    config = function ()
+      require("dap-install").setup({
+        installation_path = vim.fn.stdpath("data") .. "/dapinstall/",
+      })
+    end
+  }
   use {
     'ms-jpq/chadtree',
     branch = 'chad',

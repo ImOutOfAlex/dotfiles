a66496f44833fdff814df0fc8c2a4978c49a5f5b
Alex <ImOutOfAlex@her.farm>
2023-05-12 11:37:33 -0700

    Fix s issue.

---
+++ nix/.config/home-manager/home.nix
@@ -0,0 +1,90 @@
+{ config, pkgs, ... }:
+
+{
+  home = {
+    username = "alex";
+    homeDirectory = "/home/alex";
+    stateVersion = "22.11";
+    sessionVariables.LOCALES_ARCHIVE = "${pkgs.glibcLocales}/lib/locale/locale-archive";
+
+    packages = with pkgs; [
+      ripgrep
+      dua
+      jq
+    ];
+    file = {
+      ".local/bin/s" = {
+        executable = true;
+        text = ''
+#!/usr/bin/env sh
+
+if test $# -eq 1 && test -f $1; then
+  bat $1
+  exit
+fi
+
+lsd -l $@
+          '';
+      };
+    };
+  };
+
+  services = {
+    # flameshot = {
+    #   enable = true;
+    #   settings = {
+    #     General = {
+    #       disabledTrayIcon = false;
+    #       showStartupLaunchMessage = false;
+    #       useJpgForClipboard = true;
+    #     };
+    #   };
+    # };
+  };
+
+  programs = {
+    bat.enable = true;
+    lsd.enable = true;
+    zoxide.enable = true;
+    home-manager.enable = true;
+    direnv = {
+      enable = true;
+      nix-direnv = {
+        enable = true;
+      };
+    };
+
+    starship = {
+      enable = true;
+    };
+
+    rofi = {
+      enable = true;
+      theme = "solarized";
+      extraConfig = {
+        modi = "combi";
+        combi-modi = "window,drun,ssh";
+        lines = 30;
+        show-icons = true;
+      };
+    };
+
+    bash = {
+      enable = true;
+
+      initExtra = ''
+      export PATH=$PATH:$HOME/.local/bin
+        '';
+
+      shellAliases = {
+        cat = "bat";
+        ls = "lsd";
+        ll = "lsd -al";
+        l = "lsd -l";
+        vim = "nvim";
+        vi = "nvim";
+        v = "nvim";
+      };
+    };
+  };
+}
